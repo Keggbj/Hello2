1d049dfe4b7aadcc9cb6a10af1373a121173dd80
minik <keggbj@mail.uc.edu>
2022-09-13 15:07:01 -0400

    hi

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 18;
+
+        System.out.println("your age is " + age);
+        System.out.println("Half your age is " + age / 2);
+        System.out.println("The remainder is " + age % 2);
+
+        double salary = 30000.50;
+        System.out.println("your salary is " + salary);
+        System.out.println("Half your salary is " + salary / 2);
+        System.out.println("Mod 2 salary is " + salary % 2);
+
+        // String
+
+        String name = "Ben";
+        String LName = "Kegg";
+
+        System.out.println("Your name is " + name + " " + LName);
+    }
+}
